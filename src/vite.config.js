4fe9345baa66515f5f91e981cb9a2b9d79129af9
Santiago Argilla <sargilla@gmail.com>
2023-03-06 18:53:25 -0300

    primer commit

---
+++ src/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+        },
+    },
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+        host: true,
+        watch: {
+            // https://vitejs.dev/config/server-options.html#server-watch
+            usePolling: true,
+        },
+    },
+});
